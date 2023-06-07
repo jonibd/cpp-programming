dbbd09da34cc057dba35a078f481f081e61b5976
MD. Jony Islam <135750851+jonibd@users.noreply.github.com>
2023-06-07 06:51:14 +0600

    Add files via upload

---
+++ problem6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float A,B;
+    cin>>A>>B;
+    double MEDIA = (A*3.5+B*7.5)/(7.5+3.5);
+    printf("MEDIA = %.5lf\n",MEDIA);
+    return 0;
+
+}
